7721623f0de364424a40e2b7c7e2efac45397f81
jaimemin <jaimemin@gmail.com>
2021-10-16 00:30:11 +0900

    JPA Subquery

--- src/main/java/com/tistory/jaimemin/jpql/JpaMain.java
+++ src/main/java/com/tistory/jaimemin/jpql/JpaMain.java
@@ -28,28 +28,23 @@ public class JpaMain {
             entityManager.flush();
             entityManager.clear();
 
-            String query = "SELECT m FROM Member m INNER JOIN m.team t";
-            List<Member> members = entityManager.createQuery(query, Member.class)
+            /**
+             * select
+             *                 (select
+             *                     avg(cast(member2_.age as double))
+             *             from
+             *                 Member member2_) as col_0_0_
+             *         from
+             *             Member member0_
+             *         inner join
+             *             Team team1_
+             *                 on (
+             *                     member0_.username=team1_.name
+             *                 )
+             */
+            String query = "SELECT (SELECT AVG(m1.age) FROM Member m1) as avgAge FROM Member m JOIN Team t on m.username = t.name";
+            List<Double> ages = entityManager.createQuery(query, Double.class)
                             .getResultList();
-
-            String query2 = "SELECT m FROM Member m LEFT OUTER JOIN m.team t";
-            List<Member> leftMembers = entityManager.createQuery(query2, Member.class)
-                    .getResultList();
-
-            // cross join
-            String query3 = "SELECT m FROM Member m, Team t WHERE m.username = t.name";
-            List<Member> thetaMembers = entityManager.createQuery(query3, Member.class)
-                    .getResultList();
-
-            // 조인 대상 필터링
-            String query4 = "SELECT m FROM Member m LEFT JOIN m.team t on t.name = 'teamA'";
-            List<Member> filteringMembers = entityManager.createQuery(query4, Member.class)
-                            .getResultList();
-
-            // 연관관계가 없는 엔티티 외부 조인
-            String query5 = "SELECT m FROM Member m LEFT JOIN Team t on m.username = t.name";
-            List<Member> notRelatedMembers = entityManager.createQuery(query5, Member.class)
-                    .getResultList();
 
             transaction.commit();
         } catch (Exception e) {

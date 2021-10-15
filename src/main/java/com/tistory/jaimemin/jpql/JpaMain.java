a0c3a693a0869df89cdba91a2cc05bf2f766d862
jaimemin <jaimemin@gmail.com>
2021-10-16 01:09:38 +0900

    사용자 정의 함수

--- src/main/java/com/tistory/jaimemin/jpql/JpaMain.java
+++ src/main/java/com/tistory/jaimemin/jpql/JpaMain.java
@@ -14,52 +14,26 @@ public class JpaMain {
         transaction.begin();
 
         try {
-            Team team = new Team();
-            team.setName("team");
-            entityManager.persist(team);
+            Member member1 = new Member();
+            member1.setUsername("관리자1");
 
-            Member member = new Member();
-            member.setUsername("member");
-            member.setAge(10);
-            member.setTeam(team);
-            member.setMemberType(MemberType.ADMIN);
+            entityManager.persist(member1);
 
-            entityManager.persist(member);
+            Member member2 = new Member();
+            member2.setUsername("관리자2");
+
+            entityManager.persist(member2);
 
             entityManager.flush();
             entityManager.clear();
 
-            // QueryDSL로는 쉽게 작성 가능
-            // String add는 힘듬
-            String query = "SELECT " +
-                    "CASE WHEN m.age <= 10 then '학생요금' " +
-                    "WHEN m.age >= 60 then '경로요금' " +
-                    "ELSE '일반요금' " +
-                    "END " +
-                    "FROM Member m";
+            String query = "SELECT group_concat(m.username) FROM Member m";
             List<String> result = entityManager.createQuery(query, String.class)
-                    .getResultList();
+                            .getResultList();
 
+            // s = 관리자1,관리자2
             for (String s : result) {
                 System.out.println("s = " + s);
-            }
-
-            // coalsece
-            String query2 = "SELECT COALESCE(m.username, '이름 없는 회원') as username FROM Member m";
-            List<String> result2 = entityManager.createQuery(query2, String.class)
-                            .getResultList();
-
-            for (String s : result2) {
-                System.out.println("s2 = " + s);
-            }
-
-            // NULLIF
-            String query3 = "SELECT NULLIF(m.username, 'member') FROM Member m";
-            List<String> result3 = entityManager.createQuery(query3, String.class)
-                            .getResultList();
-
-            for (String s : result3) {
-                System.out.println("s3 = " + s);
             }
 
             transaction.commit();
